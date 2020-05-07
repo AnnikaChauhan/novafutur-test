780ea0e86b97dc5d9a84eb6c6a1c5c26f93477f2
Annika Chauhan <annikac0711@gmail.com>
2020-05-07 20:26:24 +0100

    fancy fetch done and working

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styles from './App.module.scss';
 import CurrentWeather from "../../components/CurrentWeather";
 import ProgressBar from "../../components/ProgressBar/ProgressBar";
+import Forecasts from '../../components/Forecasts/Forecasts';
 
 export default class App extends Component {
   state = {
@@ -11,7 +12,7 @@ export default class App extends Component {
     appid: "9b54ffa13641d002a298819da9bc60a9",
     currentWeatherURL: "https://api.openweathermap.org/data/2.5/weather?",
     currentWeather: "",
-    forecastWeatherURL: "api.openweathermap.org/data/2.5/forecast?",
+    forecastWeatherURL: "https://api.openweathermap.org/data/2.5/forecast?",
     forecastWeather: ""
   }
 
@@ -28,13 +29,25 @@ export default class App extends Component {
   }
 
   fetchWeather = () => {
-    fetch(`${this.state.currentWeatherURL}q=${this.state.city}&appid=${this.state.appid}`)
-      .then(response => response.json())
-      .then(currentWeather => {
+    let currentWeatherURL = `${this.state.currentWeatherURL}q=${this.state.city}&appid=${this.state.appid}`;
+    let forecastWeatherURL = `${this.state.forecastWeatherURL}q=${this.state.city}&appid=${this.state.appid}`;
+    Promise.all([fetch(currentWeatherURL), fetch(forecastWeatherURL)])
+      .then(([res1, res2]) => {
+        return Promise.all([res1.json(), res2.json()])
+      })
+      .then(([res1, res2]) => {
         this.setState({
-          currentWeather
+          currentWeather: res1,
+          forecastWeather: res2
         })
       })
+    // fetch(`${this.state.currentWeatherURL}q=${this.state.city}&appid=${this.state.appid}`)
+    //   .then(response => response.json())
+    //   .then(currentWeather => {
+    //     this.setState({
+    //       currentWeather
+    //     })
+    //   })
   }
 
   componentDidMount = () => {
@@ -56,10 +69,12 @@ export default class App extends Component {
 
   render() {
     console.log(this.state.currentWeather);
+    console.log(this.state.forecastWeather);
     return (
       <div className={styles.appDarkMode}>
         <CurrentWeather city={this.state.city} weather={this.state.currentWeather} time={this.state.time} />
         <ProgressBar timer={this.state.timer} />
+        <Forecasts />
       </div>
     );
   }
